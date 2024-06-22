16db4d8f1cb0baf0ab9cfab14d77f2dbaf469251
Victor He <81502599+VictorHeDev@users.noreply.github.com>
2024-06-21 20:04:38 -0400

    Create fork, and add plugins for oil, theme, undotree. Harpoon is added, but commented out. (#1)

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,14 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.backup = false
+
+-- allow undotree to have acces to undos from days ago
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undotree"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.colorcolumn = "120"
